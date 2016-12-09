32f3d55abd9caef1a6086acc501c58a9aeaedb1a
zachary_diemer <zdiemer21@gmail.com>
2016-12-09 04:28:47 -0500

    Added Mongo to node, and connected the application to the database

--- server.js
+++ server.js
@@ -5,6 +5,25 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+app.get('/', (req,res) => {
+	res.sendfile('index.html');
+});
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, (err, db) => {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  
+  db.close();
+});
+
+
 app.listen('5001', () => {
 	console.log("My App works WOOOO");
 });
